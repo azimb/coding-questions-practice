7492418188da058fee616c11fb4d7413ecc4f8a9
Azim Baghadiya <khushboo.baghadiya@ircoder.com>
2019-05-23 01:27:34 -0400

    added code for len>2

--- coding-questions-python/longest-palindromic-substring.py
+++ coding-questions-python/longest-palindromic-substring.py
@@ -7,9 +7,13 @@ Dynamic programming -- maintain a 2D array that stores a boolean at each index
 This way, we can use pre-computed results of smaller lengths to find the current result for the (current) lengths
 '''
 
+longestLength = -1
+longestSubstring = ""
+
+def init():
+	print(dummy)
+
 def longestPalindromicSubstring(str):
-	longest_palindromic_substring = "";
-	length_of_longest = 0;
 	
 	cache = []
 	
@@ -22,26 +26,37 @@ def longestPalindromicSubstring(str):
 	#set the values for substrings of length 1 and 2
 	for i in range(len(cache)):
 		cache[i][i] = True
+		setSubstring(str, i, i)
 		
 		if (i != len(str)-1) and (str[i] == str[i+1]):
 			cache[i][i+1] = True
+			setSubstring(str, i, i+1)
 			
 	#set the values for substrings of length greater than 2
+	k = 0
+	for i in range(2, len(str)):
+		for j in range(len(str) - i):
+			k = i+j
+			if str[j] == str[k] and cache[j+1][k-1] == True:
+				cache[j][k] = True
+				setSubstring(str, j, k)
+	return longestSubstring
 
-	#return cache
+def getSubstring(str, i, j):
+	return str[i:j+1]
+		
 
-def getSubstring(string, i, j):
-	return "";
+def setSubstring(str, i, j):
+	global longestLength
+	global longestSubstring
+	substring = getSubstring(str, i, j)
+	if len(substring) > longestLength:
+		longestLength = len(substring)
+		longestSubstring = substring
+
+result = longestPalindromicSubstring("jkahhakkkracecarxoxo")
+print(result)
 
 
-#result = longestPalindromicSubstring("llolk")
-#print(result)
-	
-'''	
-"llolk"
-True True False False False
-False True False False False
-False False True False False
-False False False True False
-False False False False True
-'''
+#TODO: check why global keyword is needed on ln 51, 52
+#TODO: estimate big O runtime complexity
