051f53b7d60400507014d0647756241b4a53a45b
Azim Baghadiya <abaghadiya@blackberry.com>
2019-05-26 21:45:42 -0400

    Initial commit.

---
+++ coding-questions-python/reverse-sentence.py
@@ -0,0 +1,8 @@
+'''
+Given a sentence represented by a c-string (char array), reverse the sentence in place.
+Ex: ['c','s',' ','i','s',' ','a','w','e','s','o','m','e']
+    must result in ['a','w','e','s','o','m','e',' ','i','s',' ','c','s']
+
+Idea:
+    - reverse the entire c-string
+'''
