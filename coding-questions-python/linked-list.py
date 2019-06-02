e9388f9193197306f0b393b264c0205efba3db5d
Azim Baghadiya <abaghadiya@blackberry.com>
2019-06-02 02:28:15 -0400

    Added linkedlistnode class

---
+++ coding-questions-python/linked-list.py
@@ -0,0 +1,14 @@
+'''
+User defined (singly) linked list none class.
+Each node stores a value, and a pointer to next (node).
+
+Sample usage:
+```
+second = LinkedListNode(5, None)
+head = LinkedListNode(2, second)
+```
+'''
+
+class LinkedListNode:
+    def __init__(self, val, next):
+self.val, self.next = val, next
