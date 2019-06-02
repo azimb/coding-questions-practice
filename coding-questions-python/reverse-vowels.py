94316796715db173eb5fb72389d2eb7cd5960f1e
Azim Baghadiya <abaghadiya@blackberry.com>
2019-06-02 01:10:59 -0400

    Code refactoring and added a new test case.

--- coding-questions-python/reverse-vowels.py
+++ coding-questions-python/reverse-vowels.py
@@ -5,6 +5,7 @@ Runtime: O(N)
 
 Space complexity: constant (not considering the space required for the input).
 '''
+
 def reverseVowels(str):
     if str is None:
         return None
@@ -16,17 +17,12 @@ def reverseVowels(str):
     end = len(cString) - 1
 
     while start < end:
-        if cString[start] in vowels and cString[end] in vowels:
-            cString[start], cString[end] = cString[end], cString[start]
-            start, end = start + 1, end - 1
-
-        elif cString[start] in vowels:
-            end -= 1
-
-        elif cString[end] in vowels:
+        if cString[start] not in vowels:
             start += 1
-
+        elif cString[end] not in vowels:
+            end -= 1
         else:
+            cString[start], cString[end] = cString[end], cString[start]
             start, end = start + 1, end - 1
 
     result = "".join(cString)
@@ -40,5 +36,6 @@ class TestReverseVowels(unittest.TestCase):
         self.assertEqual(reverseVowels("hello"), "holle")
         self.assertEqual(reverseVowels("egg"), "egg")
         self.assertEqual(reverseVowels("fry"), "fry")
+        self.assertEqual(reverseVowels("racecar, coding is awesome"),"recocer, cading is owasema")
         self.assertEqual(reverseVowels(""), "")
 unittest.main()
