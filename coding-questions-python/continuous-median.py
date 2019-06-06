ac439dbce2ee41f1109c3f3e0507c4715a04f7e0
Azim Baghadiya <abaghadiya@blackberry.com>
2019-06-05 20:39:52 -0400

    initial commit.

---
+++ coding-questions-python/continuous-median.py
@@ -0,0 +1,16 @@
+'''
+Question:-
+    - you are reading numbers of an array, one after the other.
+    - need a method called getMedian that gets the 'current median'.
+    (current median is the median of the numbers thus fars)
+    - after each number is read, append the current median to the resultant list
+
+Recall:
+    - median is in the middle when the list is in sorted order
+    - if there are twi midpoints, then median is the average of those two values
+'''
+
+# this method fetches the "current" median
+def getMedian(arr):
+    return 1;
+
