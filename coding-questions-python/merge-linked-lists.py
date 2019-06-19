c97c6f50c45f5ab8de410af34370846211dce388
Azim Baghadiya <abaghadiya@blackberry.com>
2019-06-19 09:09:58 -0400

    Verified correctness on leetcode.

--- coding-questions-python/merge-linked-lists.py
+++ coding-questions-python/merge-linked-lists.py
@@ -3,9 +3,7 @@ class LinkedListNode:
         self.val, self.next = val, next
 
 def merge_sorted(head_one, head_two):
-
     dummyNode = LinkedListNode(0, None)
-
     tail = dummyNode
 
     while True:
@@ -21,7 +19,6 @@ def merge_sorted(head_one, head_two):
             tail.next = head_one
             tail = tail.next
             head_one = head_one.next
-
         else:
             tail.next = head_two
             tail = tail.next
@@ -29,14 +26,22 @@ def merge_sorted(head_one, head_two):
 
     return dummyNode.next
 
+# tests
+
+#trivial test
 second_one = LinkedListNode(5, None)
 head_one = LinkedListNode(2, second_one)
-
 second_two = LinkedListNode(3, None)
 head_two = LinkedListNode(1, second_two)
-
-
 result = merge_sorted(head_one, head_two)
 while result is not None:
     print(result.val)
     result = result.next
+
+''' 
+leetcode tests
+
+All tests passed on leetcode
+Runtime: 28 ms, faster than 63.70% of Python online submissions 
+    for Merge Two Sorted Lists.
+'''
