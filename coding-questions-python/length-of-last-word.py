9ef3b942b035e59398763827155464401a05ec67
Azim Baghadiya <abaghadiya@blackberry.com>
2019-06-26 21:31:45 -0400

    Better approach to consider all possible case. Approach and runtime discussed.

--- coding-questions-python/length-of-last-word.py
+++ coding-questions-python/length-of-last-word.py
@@ -1,11 +1,53 @@
-def lengthOfLastWord(self, s):
-    # approach 1
-    # arr = s.split()
-    # return 0 if len(arr) is 0 else len(arr[len(arr)-1])
+'''
+Given a string s consists of upper/lower-case alphabets and empty space characters ' ',
+return the length of last word in the string.
 
-    # approach 2
-    s = s.strip()
-    for i in range(len(s) - 1, -1, -1):
+If the last word does not exist, return 0.
+Note: A word is defined as a character sequence consists of non-space characters only.
+
+Leetcode: https://leetcode.com/problems/length-of-last-word/
+
+Suggested approach:
+    - just stripping, splitting, and getting the length of the last index should be fine
+    - however this approach uses additional space that may be unnecessary
+
+Better approach (no additional space):
+    - iterate backwards and keep a track of the length until you see the first space that is not at the end of the sentence
+    - note that a flag can be used to check if a non space character has been seen before
+    - if the sentence contains no spaces (i.e. it's a single word), just return the length
+
+Runtime complexity:
+    - linear or O(N), as going through each char once
+
+Space complexity:
+    - constant or O(1), just using an integer to track the length
+'''
+
+def lengthOfLastWord(s):
+    length = 0
+    seenLetter = False
+    for i in range(len(s)-1, -1, -1):
         if s[i] == " ":
-            return len(s) - 1 - i
-    return len(s) if (len(s) is not 0) else 0
+            if seenLetter == True:
+                return length
+        else:
+            seenLetter = True
+            length += 1
+    return length
+
+
+# tests
+import unittest
+class TestLengthOfLastWord(unittest.TestCase):
+
+    def test_addition(self):
+        self.assertEqual(lengthOfLastWord("awesome Python"), 6)  # general case
+        self.assertEqual(lengthOfLastWord("Programming"), 11)  # case with only one word
+        self.assertEqual(lengthOfLastWord("Coding    "), 6)  # case with single word and spaces at the end
+        self.assertEqual(lengthOfLastWord("I love making software     "), 8) # case with mutliple words and spaces at the end
+        self.assertEqual(lengthOfLastWord("    "), 0)  # case with just spaces
+        self.assertEqual(lengthOfLastWord(""), 0) #emopty string
+
+unittest.main()
+
+#all leetcode tests pass
