05bd013501de59836ce37a526d215ba173c4f3df
Azim Baghadiya <abaghadiya@blackberry.com>
2019-06-28 18:48:55 -0400

    Started unit testing.

--- coding-questions-python/remove-dub-from-linked-list.py
+++ coding-questions-python/remove-dub-from-linked-list.py
@@ -13,3 +13,32 @@ def remove_duplicates(head):
         prev = cur
         cur = cur.next
 
+# tests
+import unittest
+class TestRemoveDuplicates(unittest.TestCase):
+    def test_addition(self):
+        linked_list_one = array_to_linked_list([1, 2, 3, 4, 1, 5])
+        self.assertEqual(remove_duplicates(linked_list_one) == result, True)
+
+        linked_list_one = array_to_linked_list([6, 9, 3, 2, 9, 5, 9, 6, 2])
+        result = array_to_linked_list([6, 9, 3, 2, 5])
+        self.assertEqual(remove_duplicates(linked_list_one) == result, True)
+
+
+        linked_list_one = array_to_linked_list([-7, - 8])
+        result = array_to_linked_list([-7, -8])
+        self.assertEqual(remove_duplicates(linked_list_one) == result, True)
+
+        linked_list_one = array_to_linked_list([12])
+        result = array_to_linked_list([12])
+        self.assertEqual(remove_duplicates(linked_list_one) == result, True)
+
+
+        linked_list_one = array_to_linked_list([])
+        result = array_to_linked_list([])
+        self.assertEqual(remove_duplicates(linked_list_one) == result, True)
+
+
+        linked_list_one = array_to_linked_list(None)
+        result = None
+        self.assertEqual(remove_duplicates(linked_list_one) == result, True)
