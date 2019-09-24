452f5e33889a8087827a8f6a3b1f5e01f38ff410
Azim Baghadiya <azim.baghadiya@mitel.com>
2019-09-24 10:33:25 -0400

    Intial commit.

---
+++ coding-questions-python/merge_intervals.py
@@ -0,0 +1,59 @@
+'''
+Merge Intervals
+
+Given a collection of intervals, merge all overlapping intervals.
+
+Ex1:
+Input: [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+
+Ex2:
+Input: [[1,4],[4,5]]
+Output: [[1,5]]
+Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+
+Leetcode: https://leetcode.com/problems/merge-intervals/
+
+Approach:
+- note that the intervals array is not sorted, that is, you could see something like [[1,4][0,1]]
+- so, we will use sort the intervals by the first value `interval[0]`
+- this will be done using lambda expressions
+
+- after soring, each set of intervals that can be merged will apper as a contiguous "run" in the sorted list
+
+- for each interval:
+    * if it starts after the previous (merged) interval ends, they don't overlap in this case, the interval is appended
+    * otherwise, when they overlap, we merge them by updating the end (interval[1]) of the previously merged
+        interval if it's less than the end of the current interval
+
+Time complexity:
+    - sorting will take O(nlogn) time
+    - one pass through the interval array will take O(n) time
+    - so, the time complexity is O(nlogn)
+
+Space complexity:
+    - #FIXME: is the sorting inplace?
+    - because the sorting is inplace, we don't use extra storge
+    (excluing the array that is used to return)
+    - so, the space complexity is O(1) or constant
+'''
+
+def merge_intervals(intervals):
+    # uses time slot (similar to merge sort) nlogn time and O(n) space
+    intervals.sort(key=lambda x: x[0])
+    
+    merged_intervals = []
+
+    for cur_interval in intervals:
+        # if the current intervals starts after the previously merged interal ends
+        if not merged_intervals or merged_intervals[-1][1] < cur_interval[0]:
+            merged_intervals.append(cur_interval)
+
+        # there is overlapping -- update the end of the merged interval is required
+        else:
+            merged_intervals[-1][1] = max(merged_intervals[-1][1], cur_interval[1])
+
+    return merged_intervals
+
+# all leetcode tests pass
