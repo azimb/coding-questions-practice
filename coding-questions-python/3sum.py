bac76644ba76e427b4a6a0fb0a5bbf54e96e81df
baghadiya <38919150+baghadiya@users.noreply.github.com>
2019-10-22 13:34:12 -0400

    Update 3sum.py

--- coding-questions-python/3sum.py
+++ coding-questions-python/3sum.py
@@ -13,36 +13,34 @@ Time and space complexities: O(N^2) and O(1)
 '''
 
 def threeSum(self, nums):
-output = []
-nums.sort()
+  output = []
+  nums.sort()
 
-for i in range(len(nums) - 2):
-  # if num > 0, nums after num will be > 0. No way to add them to make sum 0
-  if nums[i] > 0: break 
-  # already looked at the solution for this number, skip duplicate
-  if i > 0 and nums[i] == nums[i-1]: continue
-  # standard two sum
-  start = i+1; end = len(nums)-1; target = -1*nums[i]
-  
-  while start < end:
-    sum = nums[start] + nums[end]
+  for i in range(len(nums) - 2):
+    # if num > 0, nums after num will be > 0. No way to add them to make sum 0
+    if nums[i] > 0: break 
+    # already looked at the solution for this number, skip duplicate
+    if i > 0 and nums[i] == nums[i-1]: continue
+    # standard two sum
+    start = i+1; end = len(nums)-1; target = -1*nums[i]
 
-    if sum == target:
-      output.append( [ nums[i], nums[start], nums[end]] )
-      while start < end and nums[start] == nums[start+1]: start += 1
-      while start < end and nums[end] == nums[end-1]: end -= 1
-      start += 1; end -= 1
+    while start < end:
+      sum = nums[start] + nums[end]
 
-    elif sum > target: 
-      # skip duplicates
-      while start < end and nums[end] == nums[end-1]: end -= 1
-      end -= 1
+      if sum == target:
+        output.append( [ nums[i], nums[start], nums[end] ] )
+        while start < end and nums[start] == nums[start+1]: start += 1 # avoid duplicates
+        while start < end and nums[end] == nums[end-1]: end -= 1 # avoid duplicates
+        start += 1; end -= 1 # look at the next pair
 
-    else:
-      # skip duplicates
-      while start < end and nums[start] == nums[start+1]: start += 1
-      start += 1
+      elif sum > target: 
+        while start < end and nums[end] == nums[end-1]: end -= 1 # skip duplicates
+        end -= 1
 
-return output
+      else:
+        while start < end and nums[start] == nums[start+1]: start += 1 # skip duplicates
+        start += 1
+
+  return output
 
 # all leetcode tests pass as of 13th Oct 2019
