e9745717a3471aba36f68dce0b31d4748d1940c0
baghadiya <38919150+baghadiya@users.noreply.github.com>
2019-10-22 13:47:28 -0400

    Update add-two-numbers.py

--- coding-questions-python/add-two-numbers.py
+++ coding-questions-python/add-two-numbers.py
@@ -1,6 +1,10 @@
 '''
-Given a string, find the length of the longest substring without repeating characters.
-Leetcode: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+You are given two non-empty linked lists representing two non-negative integers. 
+The digits are stored in reverse order and each of their nodes contain a single digit. 
+Add the two numbers and return it as a linked list.
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Leetcode: https://leetcode.com/problems/add-two-numbers/
 
 Approach:
     - iterate over the two lists simultaneously, node by node, and add the two digits (and carry)
@@ -13,7 +17,7 @@ Approach:
 
 Complexities:
     - time complexity: iterating over the two lists once, so time complexity is O(n+m) where
-        n.m are the lengths of the two linked lists
+        n,m are the lengths of the two linked lists
     - space complexity is O(1) or constant
 '''
 
@@ -27,8 +31,8 @@ def add_two_numbers(l1, l2):
 
     # keep iterating until we have reached until the end of both lists and carry is 0
     while l1_pointer or l2_pointer or carry:
-        val_one = l1_pointer.val if l1_pointer != None else 0
-        val_two = l2_pointer.val if l2_pointer != None else 0
+        val_one = l1_pointer.val if l1_pointer else 0
+        val_two = l2_pointer.val if l2_pointer else 0
         addition = val_one + val_two + carry
 
         # carry is the first digit of the two digits of the addition
@@ -36,13 +40,13 @@ def add_two_numbers(l1, l2):
         carry = addition // 10
 
         # add the second digit of the addition as a Node
-        # or the only digit, if it's a single digit addition
+        # or the only digit, if it's a single digit sum
         dummy.next = LinkedListNode(addition % 10)
         dummy = dummy.next
 
         # go to the next node in the list, if it exists
-        if l1_pointer != None: l1_pointer = l1_pointer.next
-        if l2_pointer != None: l2_pointer = l2_pointer.next
+        if l1_pointer: l1_pointer = l1_pointer.next
+        if l2_pointer: l2_pointer = l2_pointer.next
 
     return result.next
 
